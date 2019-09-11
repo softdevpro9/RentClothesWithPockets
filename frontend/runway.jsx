70d3ed53e71f9214c18dd56e9fad1d1e1ad049c9
softdevpro9 <shearinmichael0@gmail.com>
2019-09-10 18:05:26 -0700

    finished adding ajax for signup, login, logout, tested and works

--- frontend/runway.jsx
+++ frontend/runway.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from  'react-dom'
+import ReactDOM from  'react-dom';
+import {signup, login, logout} from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-
+    window.signup = signup;
+    window.login = login;
+    window.logout = logout;
+    window.fish = () => {console.log('hi');};
     const root = document.getElementById('root')
-    ReactDOM.render(<h1>HIIIIII</h1>, root)
+    ReactDOM.render(<h1>BYEEE</h1>, root)
 })
