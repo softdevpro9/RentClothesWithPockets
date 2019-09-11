dbb8b2d2f1a976c4f0ae37f6f531a3387f872eb7
softdevpro9 <shearinmichael0@gmail.com>
2019-09-11 11:09:47 -0700

    finished  adding user auth reducers, and state is flat, and hash router, root.jsx, and app.jsx files are set up

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,48 @@
+import * as UserAPIUtil from '../util/session_api_util';
+
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+const receiveCurrentUser = (currentUser) => {
+    return ({
+        type: RECEIVE_CURRENT_USER,
+        currentUser
+    })
+}
+
+const logoutCurrentUser = () => {
+    return ({
+        type: LOGOUT_CURRENT_USER
+    })
+}
+
+const receiveErrors = (errors) => {
+    return ({
+        type: RECEIVE_SESSION_ERRORS,
+        errors
+    })
+}
+
+export const signup = user => dispatch => (
+    UserAPIUtil.signup(user).then(user => (
+        dispatch(receiveCurrentUser(user))
+    ), err => (
+        dispatch(receiveErrors(err.responseJSON))
+    ))
+);
+
+export const login = user => dispatch => (
+    APIUtil.login(user).then(user => (
+        dispatch(receiveCurrentUser(user))
+    ), err => (
+        dispatch(receiveErrors(err.responseJSON))
+    ))
+);
+
+export const logout = () => dispatch => (
+    APIUtil.logout().then(user => (
+        dispatch(logoutCurrentUser())
+    ))
+);
--- frontend/runway.jsx
+++ frontend/runway.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from  'react-dom';
+import configureStore from './store/store'
 import {signup, login, logout} from './util/session_api_util';
+import Root from './components/root'
 
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
     window.signup = signup;
     window.login = login;
     window.logout = logout;
     window.fish = () => {console.log('hi');};
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     const root = document.getElementById('root')
-    ReactDOM.render(<h1>BYEEE</h1>, root)
+    ReactDOM.render(<Root store={store}/>, root)
 })
